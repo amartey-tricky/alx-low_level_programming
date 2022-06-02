b131ac3c8f656670d3c3c6093952b248b65210eb
Tricky <trickya23@outlook.com>
2022-06-02 16:48:51 +0000

    to do later

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int quote[] = 'and that piece of art is useful\" \
+	- Dora Korpar, 2015-10-19';
+
+	putchar(quote);
+	putchar('\n');
+
+	return (0);
+}
