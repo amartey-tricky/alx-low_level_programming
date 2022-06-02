8437b1259cab818b23c4d0f19244e8e0b4d82188
Tricky <trickya23@outlook.com>
2022-06-02 16:06:34 +0000

    fix bugs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints string "with proper grammar, 
+ * main - prints string "with proper grammar,
  * but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
