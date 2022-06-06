9a78c0b7b5b0d7a1de44beb875f079eb45eab45c
Amartey Tricky <102452248+amartey-tricky@users.noreply.github.com>
2022-06-06 15:00:09 +0000

    add quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
- * main - Print "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ * main - Print exactly "and that piece of art is useful\" - Dora Korpar,\
+ *         2015-10-19" to stderr
  *
- * Return: 1
+ * Return: Always 1
  */
 int main(void)
 {
-	char quote[] = ":and that piece of art is useful\" \
-	- Dora Korpar, 2015-10-19";
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, quote, sizeof(quote));
+	fwrite(quote, sizeof(char), strlen(quote), stderr);
 
 	return (1);
 }
