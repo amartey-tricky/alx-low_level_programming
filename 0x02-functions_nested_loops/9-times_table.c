9a30b3a13de37ddd316283e430f6d26c33b5d939
Tricky <trickya23@outlook.com>
2022-06-09 07:15:17 +0000

    fix times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,33 +13,29 @@ void times_table(void)
 	{
 		for (row = 0; row < 10; row++)
 		{
-			product = column * row;
-			if (product < 10)
+			product = row * column;
+
+			if (row == 0)
 			{
+				_putchar('0' + product);
+			}
+
+			else if (product < 10)
+			{
+				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(product + '0');
 			}
 
-			else if (product < 100)
+			else
 			{
+				_putchar(',');
 				_putchar(' ');
 				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
 			}
-
-			else
-			{
-				_putchar(product / 100 + '0');
-				_putchar(product / 10 % 10 + '0');
-				_putchar(product % 10 + '0');
-			}
-
-			if (row < 9)
-				_putchar(',');
-
-			else
-				_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
