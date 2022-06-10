c10cb30adb3807f171fada4038c711b7032c7899
Tricky <trickya23@outlook.com>
2022-06-10 04:20:43 +0000

    add header

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -13,4 +13,8 @@ void positive_or_negative(int i);
 /* print the largest three integers */
 int largest_number(int a, int b, int c);
 
+/* print remaining days in the year */
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
 #endif
