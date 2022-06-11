427b9c25b92f23846a34b78040e65dac29b828ec
Tricky <trickya23@outlook.com>
2022-06-11 06:14:37 +0000

    is upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/* Function that checks for uppercase characters */
+int _isupper(int c);
+
+#endif
