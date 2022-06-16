2e4850c9082ecdfd84358942ffce5c8d2373c411
amartey-tricky <trickya23@outlook.com>
2022-06-16 10:26:07 +0000

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies the string pointed to by src to dst
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,4 +16,7 @@ char *_strcat(char *dest, char *src);
 /* function that concatenates two strings */
 char *_strncat(char *dest, char *src, int n);
 
+/* function that copies a string */
+char *_strncpy(char *dest, char *src, int n);
+
 #endif
