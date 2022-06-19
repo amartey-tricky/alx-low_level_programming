963b2e5e1f972eef66c7331f29a2a3569f283f22
Tricky <trickya23@outlook.com>
2022-06-19 21:31:58 +0000

    mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,18 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+
+	char mozart[] = {'A', 'E', 'O', 'T', 'L'};
 
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == mozart[j] || str[i] - 32 == mozart[j])
+				str[i] = j + '0';
+		}
 		i++;
 	}
 
