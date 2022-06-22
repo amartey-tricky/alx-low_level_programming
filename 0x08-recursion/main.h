278b17ad7f6234e505e0801963cc51f484fcfb82
Tricky <trickya23@outlook.com>
2022-06-22 22:30:20 +0000

    pow recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -22,4 +22,7 @@ int _strlen_recursion(char *s);
 /* function that returns the factorial of a given number */
 int factorial(int n);
 
+/* function that return the value of x raised to the power of y */
+int _pow_recursion(int x, int y);
+
 #endif
