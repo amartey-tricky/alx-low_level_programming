9e59fed4d262f019c75203359500b9703e07c446
amartey-tricky <trickya23@outlook.com>
2022-06-22 15:42:00 +0000

    put rev recurssion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse followed by a new line
+ * @s: pointer to string to be reversed
+ *
+ * Return: reversed string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);	
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,4 +13,7 @@ int _putchar(char c);
 /* function that prints a string followed by a new line */
 void _puts_recursion(char *s);
 
+/* function that prints a tring in reverse followed by anew line */
+void _print_rev_recursion(char *s);
+
 #endif
