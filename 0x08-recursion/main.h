12bd772ead304d5f914a4db133207eaba7e86614
Tricky <trickya23@outlook.com>
2022-06-22 21:51:44 +0000

    factorial

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,10 +13,13 @@ int _putchar(char c);
 /* function that prints a string followed by a new line */
 void _puts_recursion(char *s);
 
-/* function that prints a tring in reverse followed by anew line */
+/* function that prints a string in reverse followed by anew line */
 void _print_rev_recursion(char *s);
 
 /* function that returns the length of a string */
 int _strlen_recursion(char *s);
+
+/* function that returns the factorial of a given number */
+int factorial(int n);
 
 #endif
