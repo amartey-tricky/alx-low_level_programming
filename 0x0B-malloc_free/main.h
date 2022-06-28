a5e2c6ef06e49d255935bd47317739974f4b8119
Tricky <trickya23@outlook.com>
2022-06-28 18:17:58 +0000

    create array

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/* _puchar function */
+int _putchar(char c);
+
+/* function that creates an array of chars and initializes with a specific char */
+char *create_array(unsigned int size, char c);
+
+#endif
