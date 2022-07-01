46fcaac56b6a973c31360bac62724dcbca71dee1
Tricky <trickya23@outlook.com>
2022-07-01 06:48:49 +0000

    malloc checked

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <limits.h>
+
+/* _putchar function */
+int _putchar(char c);
+
+/* function that allocates memory using malloc */
+void *malloc_checked(unsigned int b);
+
+#endif
