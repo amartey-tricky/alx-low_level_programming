df0fb7eb5d8139560c3c840ab3f6ce5adae7fbdb
amartey-tricky <trickya23@outlook.com>
2022-07-04 15:30:06 +0000

    dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,36 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - struct for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct for dog
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * struct dog_t - create a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: create a new dog
+ * Return: pointer to the new dog
+ */
+typedef struct dog_t{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
