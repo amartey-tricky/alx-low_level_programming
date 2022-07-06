ee52e8c53a6e092ea35870ca9f8e663174872fbb
amartey-tricky <trickya23@outlook.com>
2022-07-06 13:22:18 +0000

    fix bugs

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,13 +14,26 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("Error: three arguments required\n");
-		return (1);
+		printf("Error\n");
+		return (98);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	op = argv[2];
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	/* divising by 0 error */
+	if ((op[0] == '/' && b == 0) || (op[0] == '%' && b == 0))
+	{
+		printf("Error\n");
+		return (98);
+	}
 
 	res = get_op_func(op)(a, b);
 
