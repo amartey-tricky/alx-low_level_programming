80b01a786e23b3999fa77f4aeaabbfd45ea1e442
Tricky <trickya23@outlook.com>
2022-07-07 03:41:04 +0000

    fix header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -14,8 +14,10 @@ int _putchar(char c);
 /* function that prints a name */
 void print_name(char *name, void (*f)(char *));
 
-/** function that executes a function given as a parameter on
- *  each element of an array */
+/**
+ * function that executes a function given as a parameter on
+ *  each element of an array
+ */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 /* function that searches for an integer */
