7ca7e45b2958dcead26483b3c6375b61609616a3
amartey-tricky <trickya23@outlook.com>
2022-07-07 14:23:04 +0000

    fix print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,6 +13,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
+	if (separator == NULL)
+		return;
+
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
