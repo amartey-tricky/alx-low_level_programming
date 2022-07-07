d9a6b8023ad32b664b0c768ae032261d922448af
Tricky <trickya23@outlook.com>
2022-07-07 04:43:18 +0000

    done

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
+{
+	int i, n;
+	int (*p)(int, char **);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	p = &main;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2x ", *((unsigned char *)(p + i)));
+	}
+	printf("\n");
+
+	return (0);
+}
