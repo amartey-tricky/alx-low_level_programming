7cd5f547b5c6e820f59adc6b9d04ffa1b10fa5b7
amartey-tricky <trickya23@outlook.com>
2022-07-14 16:36:55 +0000

    fix betty

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,39 +8,14 @@
  *
  * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 	int bytes, index;
-	char (*buffer)(int, char **) = main;
-	unsigned char opcode;
+	int (*f)(int, char *);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	for (index = 0; index < bytes; index++)
-	{
-		opcode = (unsigned char *)buffer;
-		printf("%.2x\n", opcode);
-
-		if (index == bytes - 1)
-			continue;
-
-		printf(" ");
-
-		buffer++;
-	}
-
-	printf("\n");
-
-	return (0);
 }
