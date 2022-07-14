c180ea22d9d28dffb3040a2e83a1973bb17b5421
Tricky <trickya23@outlook.com>
2022-07-14 16:28:40 +0000

    fix bugs

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,10 +11,15 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 
 	while (h != NULL)
-	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		count++;
-	}
-	return (count);
+        {
+                if (h->str == NULL)
+                        printf("[0] (nil)\n");
+
+                else
+                        printf("[%u] %s\n", h->len, h->str);
+
+                h = h->next;
+                count++;
+        }
+        return (count);
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,24 +9,24 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+        list_t *new_node;
+        unsigned int len;
 
-	if (head == NULL || str == NULL)
-		return (NULL);
+        for (len = 0; str[len] != '\0'; len++)
+                ;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+        new_node = malloc(sizeof(list_t));
+        if (new_node == NULL)
+                return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = *head;
-	*head = new_node;
-
-	return (new_node);
+        new_node->str = strdup(str);
+        if (new_node->str == NULL)
+        {
+                free(new_node);
+                return (NULL);
+        }
+        new_node->len = len;
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
 }
