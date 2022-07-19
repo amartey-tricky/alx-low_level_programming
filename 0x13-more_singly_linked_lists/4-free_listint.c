3623d7d8fa188eabde6f044cf29cfd9f344951da
Tricky <trickya23@outlook.com>
2022-07-19 09:47:46 +0000

    fix free list int

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,10 +10,13 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	if ((temp = head) != NULL)
+	if (head == NULL)
+		free(head);
+
+	while (head != NULL)
 	{
+		temp = head;
 		head = head->next;
 		free(temp);
 	}
-
 }
